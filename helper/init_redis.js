2692f1607029a422ad0d0b2ea048ac5a2c9e90d7
narendraiiitl <lcs2019015@iiitl.ac.in>
2020-09-05 22:24:41 +0530

    redis issue resolved

--- helper/init_redis.js
+++ helper/init_redis.js
@@ -1,8 +1,5 @@
 const redis = require('redis');
-const client = redis.createClient({
-    port:6379,
-    host:"127.0.0.1"
-})
+const client = redis.createClient()
 client.on('connect',()=>{
     console.log("client connected to redis");
 })
