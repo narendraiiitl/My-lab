492883f6469d97ef5b679cf9a4a657f41a730043
narendraiiitl <lcs2019015@iiitl.ac.in>
2020-09-05 23:07:06 +0530

    redis

--- helper/init_redis.js
+++ helper/init_redis.js
@@ -1,5 +1,6 @@
+var Redis = require('ioredis');
 const redis = require('redis');
-const client = redis.createClient()
+const client = redis.createClient(process.env.REDIS_URL)
 client.on('connect',()=>{
     console.log("client connected to redis");
 })
